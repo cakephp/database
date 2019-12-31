Alex-Au1/cakephp

Renamed database schema dialect classes

src/Database/Driver.php
@@ -16,8 +16,8 @@ declare(strict_types=1);
  */
 namespace Cake\Database;
 
+use Cake\Database\Dialect\SchemaDialect;
 use Cake\Database\Exception\MissingConnectionException;
-use Cake\Database\Schema\BaseSchema;
 use Cake\Database\Schema\TableSchema;
 use Cake\Database\Statement\PDOStatement;
 use Closure;
@@ -270,7 +270,7 @@ abstract class Driver implements DriverInterface
     /**
      * @inheritDoc
      */
-    abstract public function schemaDialect(): BaseSchema;
+    abstract public function schemaDialect(): SchemaDialect;
 
     /**
      * @inheritDoc

src/Database/DriverInterface.php
@@ -16,7 +16,7 @@ declare(strict_types=1);
  */
 namespace Cake\Database;
 
-use Cake\Database\Schema\BaseSchema;
+use Cake\Database\Dialect\SchemaDialect;
 use Cake\Database\Schema\TableSchema;
 use Closure;
 
@@ -180,9 +180,9 @@ interface DriverInterface
      * If all the tables that use this Driver specify their
      * own schemas, then this may return null.
      *
-     * @return \Cake\Database\Schema\BaseSchema
+     * @return \Cake\Database\Dialect\SchemaDialect
      */
-    public function schemaDialect(): BaseSchema;
+    public function schemaDialect(): SchemaDialect;
 
     /**
      * Quotes a database identifier (a column name, table name, etc..) to

cakephp/database

Driver.php
@@ -17,7 +17,7 @@ declare(strict_types=1);
 namespace Cake\Database;
 
 use Cake\Database\Exception\MissingConnectionException;
-use Cake\Database\Schema\BaseSchema;
+use Cake\Database\Schema\SchemaDialect;
 use Cake\Database\Schema\TableSchema;
 use Cake\Database\Statement\PDOStatement;
 use Closure;
@@ -270,7 +270,7 @@ abstract class Driver implements DriverInterface
     /**
      * @inheritDoc
      */
-    abstract public function schemaDialect(): BaseSchema;
+    abstract public function schemaDialect(): SchemaDialect;
 
     /**
      * @inheritDoc

DriverInterface.php
@@ -16,7 +16,7 @@ declare(strict_types=1);
  */
 namespace Cake\Database;
 
-use Cake\Database\Schema\BaseSchema;
+use Cake\Database\Schema\SchemaDialect;
 use Cake\Database\Schema\TableSchema;
 use Closure;
 
@@ -180,9 +180,9 @@ interface DriverInterface
      * If all the tables that use this Driver specify their
      * own schemas, then this may return null.
      *
-     * @return \Cake\Database\Schema\BaseSchema
+     * @return \Cake\Database\Schema\SchemaDialect
      */
-    public function schemaDialect(): BaseSchema;
+    public function schemaDialect(): SchemaDialect;
 
     /**
      * Quotes a database identifier (a column name, table name, etc..) to